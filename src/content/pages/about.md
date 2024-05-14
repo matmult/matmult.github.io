a74ffcc37f90a4e7ce7135a47aff39a1bc7e9199
matmult <jmamaa@proton.me>
2024-05-14 10:31:59 -0700

    archive posts and v1 done

--- src/content/pages/about.md
+++ src/content/pages/about.md
@@ -8,13 +8,15 @@ seo:
     alt: A person sitting at a desk in front of a computer
 ---
 
-![Alt text for image](/about.jpeg)
+<!-- ![Alt text for image](/about.jpeg) -->
 
-**Note!:** This about page is entirely fictional and created for the purpose of showcasing [Dante Astro.js theme functionality](https://justgoodui.com/astro-themes/dante/).
+<!-- **Note!:** This about page is entirely fictional and created for the purpose of showcasing [Dante Astro.js theme functionality](https://justgoodui.com/astro-themes/dante/). -->
 
-## Ethan Donovan - Web Developer based in Estonia
+## matmult
 
-**Greetings!** I'm Ethan Donovan, a passionate web developer residing in the picturesque landscapes of [Estonia](https://en.wikipedia.org/wiki/Estonia). With a keen eye for design and a love for crafting seamless digital experiences, I bring a unique blend of creativity and technical expertise to the world of web development.
+_"The people who are crazy enough to think they can change the world are the ones who do."_
+
+<!-- **Greetings!** I'm Ethan Donovan, a passionate web developer residing in the picturesque landscapes of [Estonia](https://en.wikipedia.org/wiki/Estonia). With a keen eye for design and a love for crafting seamless digital experiences, I bring a unique blend of creativity and technical expertise to the world of web development.
 
 ## Skills and Expertise
 
@@ -38,6 +40,4 @@ I believe in the power of collaboration and effective communication. Whether wor
 
 ## Get in Touch
 
-Are you looking to elevate your online presence or bring your digital ideas to life? I'd love to hear from you! Feel free to reach out for collaboration, consultation, or just a friendly chat about all things web development.
-
-_Let's build something amazing together!_
+Are you looking to elevate your online presence or bring your digital ideas to life? I'd love to hear from you! Feel free to reach out for collaboration, consultation, or just a friendly chat about all things web development. -->
--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -38,12 +38,12 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
-    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
+    title: 'matmult',
+    // subtitle: 'matmult',
+    description: `matmult's blog`,
     image: {
         src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
+        alt: 'main'
     },
     headerNavLinks: [
         {
@@ -51,69 +51,74 @@ const siteConfig: SiteConfig = {
             href: '/'
         },
         {
-            text: 'Projects',
-            href: '/projects'
+            text: 'About',
+            href: '/about'
         },
         {
             text: 'Blog',
             href: '/blog'
         },
         {
-            text: 'Tags',
-            href: '/tags'
+            text: 'Projects',
+            href: '/projects'
         }
+        // {
+        //     text: 'Tags',
+        //     href: '/tags'
+        // }
     ],
-    footerNavLinks: [
-        {
-            text: 'About',
-            href: '/about'
-        },
-        {
-            text: 'Contact',
-            href: '/contact'
-        },
-        {
-            text: 'Terms',
-            href: '/terms'
-        },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
-        }
-    ],
-    socialLinks: [
-        {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
-        },
-        {
-            text: 'Instagram',
-            href: 'https://instagram.com/'
-        },
-        {
-            text: 'X/Twitter',
-            href: 'https://twitter.com/'
-        }
-    ],
+    // footerNavLinks: [
+    //     {
+    //         text: 'About',
+    //         href: '/about'
+    //     },
+    //     {
+    //         text: 'Contact',
+    //         href: '/contact'
+    //     }
+    //     {
+    //         text: 'Terms',
+    //         href: '/terms'
+    //     },
+    //     {
+    //         text: 'Download theme',
+    //         href: 'https://github.com/JustGoodUI/dante-astro-theme'
+    //     }
+    // ],
+    // socialLinks: [
+    //     {
+    //         text: 'Dribbble',
+    //         href: 'https://dribbble.com/'
+    //     },
+    //     {
+    //         text: 'Instagram',
+    //         href: 'https://instagram.com/'
+    //     },
+    //     {
+    //         text: 'X/Twitter',
+    //         href: 'https://twitter.com/'
+    //     }
+    // ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
-        image: {
-            src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
-        },
+        // title: 'Hi There & Welcome to My Corner of the Web!',
+        // text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        text: `As a dedicated machine learning researcher, I am deeply immersed in the exploration and development of advanced algorithms and models that push the boundaries of artificial intelligence. My work involves rigorous experimentation, data analysis, and the application of cutting-edge techniques to solve complex problems. I leverage a robust understanding of mathematics, statistics, and computer science to innovate and optimize machine learning systems. My research often intersects with various domains,including natural language processing, computer vision, and reinforcement learning, aiming to create intelligent systems that can learn and adapt autonomously.`,
+        // image: {
+        //     src: '/hero.jpeg',
+        //     alt: 'A person sitting at a desk in front of a computer'
+        // },
         actions: [
             {
-                text: 'Get in Touch',
-                href: '/contact'
+                text: 'About',
+                href: '/about'
             }
         ]
     },
-    subscribe: {
-        title: 'Subscribe to Dante Newsletter',
-        text: 'One update per week. All the latest posts directly in your inbox.',
-        formUrl: '#'
-    },
+    // subscribe: {
+    //     title: 'Subscribe to Dante Newsletter',
+    //     text: 'One update per week. All the latest posts directly in your inbox.',
+    //     formUrl: '#'
+    // },
     postsPerPage: 8,
     projectsPerPage: 8
 };
